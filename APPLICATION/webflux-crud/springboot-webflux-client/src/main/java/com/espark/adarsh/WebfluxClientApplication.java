a45da5991618407c75b06a59dae348f9dd133978
adarsh <us-guest@USMB113823-2.local>
2020-09-21 22:30:43 -0500

    spring webflux crud

--- APPLICATION/webflux-crud/springboot-webflux-client/src/main/java/com/espark/adarsh/WebfluxClientApplication.java
+++ APPLICATION/webflux-crud/springboot-webflux-client/src/main/java/com/espark/adarsh/WebfluxClientApplication.java
@@ -1,27 +1,36 @@
 package com.espark.adarsh;
 
-import com.espark.adarsh.services.ServerService;
-import org.springframework.boot.CommandLineRunner;
+import com.espark.adarsh.bean.EventBean;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.web.reactive.function.client.WebClient;
 
 
+@Slf4j
 @EnableAsync
 @SpringBootApplication
 public class WebfluxClientApplication {
 
 
-	public static void main(String[] args) {
-		SpringApplication.run(WebfluxClientApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(WebfluxClientApplication.class, args);
+    }
 
+    @Bean
+    WebClient webClient() {
+        return WebClient.create("http://localhost:8080/api");
+    }
 
-	@Bean
-	CommandLineRunner commandLineRunner(ServerService serverService){
-		return args -> {
-			serverService.serverLiveStatus();
-		};
-	}
+    @Bean
+    ApplicationRunner runner(WebClient webClient) {
+        return args -> webClient.get()
+                .uri("/event")
+                .retrieve()
+                .bodyToFlux(EventBean.class)
+                .subscribe(data -> log.info(data.toString()));
+    }
 }
